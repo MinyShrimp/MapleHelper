ed1e3c7022081478176ebfbefa2174654f2e422f
MinyShrimp <ksk7584@gmail.com>
2023-10-20 15:59:35 +0900

    프로젝트 생성

---
+++ src/main/kotlin/git/shrimp/maple_helper/MapleHelperApplication.kt
@@ -0,0 +1,11 @@
+package git.shrimp.maple_helper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapleHelperApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapleHelperApplication>(*args)
+}
---
+++ src/test/kotlin/git/shrimp/maple_helper/MapleHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.shrimp.maple_helper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapleHelperApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
