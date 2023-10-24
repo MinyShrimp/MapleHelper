f0a840f5aec970bb79c0d88ddc1dd409124b388f
MinyShrimp <ksk7584@gmail.com>
2023-10-24 18:09:16 +0900

    DiscordBot Dependency 교체

---
+++ src/main/kotlin/git/shrimp/maple_helper/discordbot/ability/MapleAbilityListener.kt
@@ -0,0 +1,23 @@
+package git.shrimp.maple_helper.discordbot.ability
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+
+class MapleAbilityListener : ListenerAdapter() {
+    private val logger = KotlinLogging.logger {}
+
+    override fun onMessageReceived(
+        event: MessageReceivedEvent
+    ) {
+        val message = event.message
+        this.logger.info { "Received message: ${message.contentRaw}" }
+        if (message.author.isBot) return
+
+        if (message.contentRaw.startsWith("!echo")) {
+            val msg = message.contentRaw.replace("!echo ", "")
+            event.channel.sendMessage(msg).queue()
+        }
+    }
+}
--- src/main/kotlin/git/shrimp/maple_helper/discordbot/global/config/DiscordBotConfig.kt
+++ src/main/kotlin/git/shrimp/maple_helper/discordbot/global/config/DiscordBotConfig.kt
@@ -1,9 +1,13 @@
 package git.shrimp.maple_helper.discordbot.global.config
 
-import discord4j.core.DiscordClientBuilder
-import discord4j.core.GatewayDiscordClient
-import discord4j.core.event.domain.Event
-import git.shrimp.maple_helper.discordbot.global.listener.EventListener
+import git.shrimp.maple_helper.discordbot.ability.MapleAbilityListener
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.ChunkingFilter
+import net.dv8tion.jda.api.utils.MemberCachePolicy
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,20 +15,27 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class DiscordBotConfig {
     @Bean
-    fun <T : Event> gatewayDiscordClient(
+    fun gatewayDiscordClient(
         @Value("\${discord.bot.token}") token: String,
-        eventListeners: List<EventListener<T>>
-    ): GatewayDiscordClient {
-        val client = DiscordClientBuilder.create(token).build().login().block()
-            ?: throw RuntimeException("DiscordClient is null")
-
-        eventListeners.forEach { eventListener ->
-            client.on(eventListener.getEventType())
-                .flatMap(eventListener::execute)
-                .onErrorResume(eventListener::handleError)
-                .subscribe()
-        }
-
-        return client
+    ): JDA {
+        return JDABuilder.createDefault(token)
+            .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+            .setBulkDeleteSplittingEnabled(false)
+            .setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER))
+            .setChunkingFilter(ChunkingFilter.NONE)
+            .disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING)
+            .setLargeThreshold(50)
+            .setAutoReconnect(true)
+            .setStatus(OnlineStatus.ONLINE)
+            .addEventListeners(MapleAbilityListener())
+            .enableIntents(
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_MESSAGE_TYPING
+            ).build()
     }
 }
